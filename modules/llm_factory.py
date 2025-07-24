945bba15ca3eaaa0694e3580239e144d0e7fd009
Grained.ai AnthonyF <anthony.fan@grained.ai>
2025-07-24 11:45:31 +0800

    feat: add initial website analysis module and LLM factory
    - Add new website analysis module with model and traversal components
    - Implement LLM factory for creating chat instances with configurable models
    - Include configuration files and documentation for project setup

---
+++ modules/llm_factory.py
@@ -0,0 +1,53 @@
+import yaml
+from pathlib import Path
+from configs.global_params import *
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+
+from configs.configs import *
+class LLMFactory:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(LLMFactory, cls).__new__(cls)
+            cls._instance.expense = {}
+        return cls._instance
+
+    @staticmethod
+    def create_llm_instance(model_name=None, temperature=0.95):
+        with open(Path(__file__).parent.parent / 'configs' / 'configs.yaml', 'r') as f:
+            config = yaml.safe_load(f)
+
+        if model_name is None:
+            model_name = DEFAULT_LLM_MODEL_NAME
+
+        if model_name.startswith("OLLAMA"):
+            return ChatOllama(
+                temperature=temperature,
+                model=config['LLM'][model_name]['llm_params']['model_name']
+            )
+        else:
+            return ChatOpenAI(
+                temperature=temperature,
+                model=config['LLM'][model_name]['llm_params']['model_name'],
+                openai_api_key=config['LLM'][model_name]['llm_params']['api_key'],
+                openai_api_base=config['LLM'][model_name]['llm_params']['endpoint']
+            )
+
+    @staticmethod
+    def create_vllm_instance(vendor=DEFAULT_VENDOR, model_name=VLM_DEFAULT_MODEL_NAME, temperature=0.95):
+        with open(Path(__file__).parent.parent / 'configs' / 'configs.yaml', 'r') as f:
+            config = yaml.safe_load(f)
+
+        return ChatOpenAI(
+            model=model_name,
+            openai_api_key=config['VLM'][vendor]['api_key'],
+            openai_api_base=config['VLM'][vendor]['endpoint'],
+            temperature=temperature
+        )
+
+if __name__ == "__main__":
+    ins = LLMFactory()
+    a = ins.create_llm_instance()
+    print(a.predict("HI"))
